c0e6bc093c069609967575e8e9c1a4d3fc498eda
priyanka1683 <priyanka100927@gmail.com>
2026-01-19 14:18:36 +0530

    added

--- src/myapp/app.py
+++ src/myapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from AWS!"
+    return "Hello World from AWS Elastic Beanstalk!"
 
 if __name__ == "__main__":
     app.run()
